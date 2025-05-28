a5092ac5379e9f0d44961c5627499ddedcaafdd8
Aman Sagar <amansagar102005@gmail.com>
2025-05-28 20:16:33 +0000

    var done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountId="15673"
+let accountEmail="aman@gmail.com"
+var accountpassword="222927"
+accountCity="kanpur"
+
+//accountId="6"
+accountEmail="jai@gmail.com"
+accountpassword="69"
+accountCity="Agra"
+
+console.log(accountId);
